b07c0b5cc5b5efc961a012930f7cc67501cf17e1
AloBanan0 <aleandromaccarrone@gmail.com>
2025-06-15 15:53:50 +0200

    update2.65

--- js/nav-script.js
+++ js/nav-script.js
@@ -5,7 +5,7 @@ window.onscroll = function(){
         document.getElementById("navigation").style.top = "0";
     }
     else{
-        document.getElementById("navigation").style.top = "-15svh";
+        document.getElementById("navigation").style.top = "-7.5rem";
     }
     prevScrollpos = currentScrollPos;
 }
